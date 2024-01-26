ed8498e2012641ba71818d810ddcb589b3f75b96
KairoBot <157165411+KairoBot@users.noreply.github.com>
2024-01-26 13:03:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ from datetime import datetime, timedelta, timezone
 # Read Slack webhook URLs from environment variables
 CHANNEL_1_WEBHOOK_URL = os.environ.get('CHANNEL_1_WEBHOOK_URL')
 CHANNEL_2_WEBHOOK_URL = os.environ.get('CHANNEL_2_WEBHOOK_URL')
-# Global variable to track if Channel 2 message was sent today
-channel_2_sent_today = False
+
 # Central Standard Time (CST) timezone
 CST = timezone(timedelta(hours=-6))
+
+# Global variable to track if Channel 2 message was sent today
+channel_2_sent_today = False
 
 def send_slack_message(webhook_url, message):
     payload = {
@@ -18,43 +20,32 @@ def send_slack_message(webhook_url, message):
     }
     requests.post(webhook_url, json=payload)
 
-
 def get_quote():
     response = requests.get('https://zenquotes.io/api/random')
     json_data = json.loads(response.text)
     quote = json_data[0]['q'] + " -" + json_data[0]['a']
     return quote
 
-
-
-
 def send_messages():
-    # Check the current minute and send message if it's a multiple of 15
-    now_minute = datetime.now().minute
-    if now_minute % 15 == 0:
-        # Send message to channel 1 every 15 minutes
-        print("sendmessage1")
-        send_slack_message(CHANNEL_1_WEBHOOK_URL, 'Message to Channel 1 - Every 15 minutes')
+    # Send message to channel 1 every 15 minutes
+    send_slack_message(CHANNEL_1_WEBHOOK_URL, 'Message to Channel 1 - Every 15 minutes')
 
     # Send message to channel 2 at 11:00am CST every day if not sent today
     now_cst = datetime.now(CST)
     if now_cst.hour == 11 and now_cst.minute == 0 and not channel_2_sent_today:
-        print("sendmessage2")
         quote_of_the_day = get_quote()
-        send_slack_message(CHANNEL_2_WEBHOOK_URL, get_quote())
+        send_slack_message(CHANNEL_2_WEBHOOK_URL, f'Message to Channel 2 - 11:00am CST: {quote_of_the_day}')
         # Set the flag to indicate that Channel 2 message was sent today
         channel_2_sent_today = True
 
 def send_backup_message():
-    print("sendmessage3")
-    # Send backup message to channel 2 if the original message wasn't sent at 11:00am
+    # Send backup message to channel 2 at 12:00pm CST if the original message wasn't sent at 11:00am
     now_cst = datetime.now(CST)
-    if now_cst.hour == 13 and not channel_2_sent_today:
-        send_slack_message(CHANNEL_2_WEBHOOK_URL, get_quote())
-        
+    if now_cst.hour == 12 and not channel_2_sent_today:
+        send_slack_message(CHANNEL_2_WEBHOOK_URL, 'Backup Message to Channel 2')
+
 if __name__ == "__main__":
     # Schedule the task to run every minute
-    print("sendmessage4")
     schedule.every().minute.do(send_messages)
 
     # Schedule the backup message task to run once a day at 12:00pm CST
@@ -63,7 +54,6 @@ if __name__ == "__main__":
     while True:
         schedule.run_pending()
         time.sleep(1)
-
 
 
 '''import os
