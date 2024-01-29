211b032e567cb3887086731536a88da165d3a47e
KairoBot <157165411+KairoBot@users.noreply.github.com>
2024-01-29 10:04:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import schedule
 import time
 import json
 from datetime import datetime, timedelta, timezone
-# Read Slack webhook URLs from environment variables
-CHANNEL_1_WEBHOOK_URL = os.environ.get('TEST_WEBHOOK_URL')
-CHANNEL_2_WEBHOOK_URL = os.environ.get('QUOTE_WEBHOOK_URL')
 
-# Read Slack webhook URL from environment variable
-#SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL')
+# Read Slack webhook URLs from environment variables
+SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL')  # Channel for daily message
+UPTIME_SLACK_WEBHOOK_URL = os.environ.get('UPTIME_SLACK_WEBHOOK_URL')  # Channel for uptime messages
 
 # Central Standard Time (CST) timezone
 CST = timezone(timedelta(hours=-6))
@@ -18,31 +16,37 @@ def get_quote():
     json_data = json.loads(response.text)
     quote = json_data[0]['q'] + " -" + json_data[0]['a']
     return quote
-
-
-def send_slack_message(message):
+def send_slack_message(webhook_url, message):
     payload = {'text': message}
-    requests.post(CHANNEL_1_WEBHOOK_URL, json=payload)
+    requests.post(webhook_url, json=payload)
 
-def send_daily_message():
+def get_daily_message():
     now_cst = datetime.now(CST)
     if now_cst.hour == 11 and now_cst.minute == 0:
-        send_slack_message(get_quote())
+        return 'Daily message at 11:00am CST'
 
-def send_uptime_message():
-    now_cst = datetime.now(CST)
-    payload = {'text': now_cst}
-    requests.post(CHANNEL_1_WEBHOOK_URL, json=payload)
+    return None
 
+def get_uptime_message():
+    now_minute = datetime.now().minute
+    if now_minute % 15 == 0:
+        return f'Uptime message at {now_minute} minute mark on the hour'
 
+    return None
 
-print("finally")
-# Schedule the task to run every minute
-schedule.every().minute.do(send_daily_message)
-schedule.every(15).minutes.do(send_uptime_message)
-while True:
-    schedule.run_pending()
+if __name__ == "__main__":
+    # Run the scheduled tasks every minute
+    while True:
+        daily_message = get_daily_message()
+        uptime_message = get_uptime_message()
 
+        if daily_message:
+            send_slack_message(SLACK_WEBHOOK_URL, daily_message)
+
+        if uptime_message:
+            send_slack_message(UPTIME_SLACK_WEBHOOK_URL, uptime_message)
+
+        time.sleep(60)
 '''import os
 import slack_sdk
 from flask import Flask
