77edbf98bcdbb57fc0e537b6155a457c80549220
KairoBot <157165411+KairoBot@users.noreply.github.com>
2024-01-29 09:42:22 -0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,4 @@
-from flask import Flask
+'''from flask import Flask
 from threading import Thread
 
 app = Flask('')
@@ -13,3 +13,4 @@ def run():
 def keep_alive():
   t = Thread(target=run)
   t.start()
+'''
